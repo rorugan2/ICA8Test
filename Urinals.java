0d0004f4d7377b783880577977b50f5ccb686b29
rorugan2 <rorugan2@DESKTOP-M7P270P>
2022-10-27 23:19:01 -0700

    updated main and test

---
+++ Urinals.java
@@ -0,0 +1,90 @@
+package org.example;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+
+public class Urinals {
+    public static void main(String[] args){
+        // int i= getUrinals(null);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("for input from file press  0 or for input from console press 1");
+        int st = sc.nextInt();
+        if(st==1){
+            int i= getUrinals(null);
+            System.out.println(i);
+
+
+        }
+        else{
+            BufferedReader br = null;
+            try {
+                FileReader file = new FileReader(new File("C:\\Users\\rorugan2\\IdeaProjects\\ICATEST8\\src\\main\\java\\org\\example\\urinal.dat"));
+                br = new BufferedReader(file);
+                //int j=0, c=0;
+                File fout = new File("rule.txt");
+                if(!fout.exists()) {
+                    fout.createNewFile();
+
+                }
+
+                FileWriter fw = new FileWriter(fout.getAbsolutePath());
+                BufferedWriter bw = new BufferedWriter(fw);
+                while (true) {
+                    String temp = br.readLine();
+                    if(temp == null) break;
+                    int val = getUrinals(temp);
+                    //    System.out.println(val);
+                    bw.write(Integer.toString(val));
+                    bw.newLine();
+                }
+                br.close();
+                bw.close();
+            } catch(Exception e) {
+                System.out.println(e);
+                // i=getUrinals(null);
+            }
+        }
+    }
+
+    public static int getUrinals(String in) {
+        String s = in;
+        if(in != null) {
+            s = in;
+        } else {
+            Scanner sc = new Scanner(System.in);
+            System.out.println(" enter the input");
+            s=sc.nextLine();
+        }
+        char[] str= new char[s.length()];
+        for(int i=0;i< s.length();i++) {
+            str[i]=s.charAt(i);
+        }
+        for(int i=1;i<str.length;i++) {
+            if(str[i-1] == '1' && str[i] == '1') {
+                return -1;
+            }
+        }
+        if(!(s.contains("1"))){
+            return (s.length()+1)/2;
+        }
+        int cc = 0, ff=0,count =0;
+        for(int i=0;i<str.length;i++) {
+            if(str[i]=='0') {
+                cc++;
+            }
+            else {
+                count = count + (cc-ff)/2;
+                ff=1;
+                cc=0;
+            }
+        }
+        count += cc/2;
+        return count;
+    }
+}
+
---
+++ UrinalsTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UrinalsTest {
+    Urinals u = new Urinals();
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void getUrinals() {
+        assertEquals(1,u.getUrinals("10001"));
+        assertEquals(0,u.getUrinals("1001"));
+        assertEquals(3,u.getUrinals("00000"));
+        assertEquals(2,u.getUrinals("0000"));
+        assertEquals(1,u.getUrinals("01000"));
+        assertEquals(-1,u.getUrinals("011"));
+    }
+}
